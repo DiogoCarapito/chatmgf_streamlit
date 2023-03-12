dc35c474578cc76af6e45cb5a8b166c83aa3492b
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-03-12 09:40:11 +0000

    starting lagnchain implementation

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 import streamlit as st
-#import openai
-import os
-
-openai_api_key = os.environ.get('OPENAI_API_KEY')
 
 st.set_page_config(
     page_title='ChatMGF',
     page_icon=':blue_book:'
 )
-#st.session_state.answer = []
+st.session_state.answer = []
 
 st.title('Chat:blue[MFG]')
 
