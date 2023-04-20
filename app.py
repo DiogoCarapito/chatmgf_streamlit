f2f9f21b287ca88952da777e8c0b07247c5b4e55
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-04-20 12:42:07 +0100

    chat upgrades and API implementation

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
+import requests
 import streamlit as st
 
-st.set_page_config(
-    page_title='ChatMGF',
-    page_icon=':blue_book:'
-)
-st.session_state.answer = []
+# Define API endpoint
+API_URL = 'https://diogocarapito-chatmgf.hf.space/run/predict'
 
-st.title('Chat:blue[MFG]')
+def get_api_response(input_text):
+    data = {"input_text": input_text}
+    response = requests.post(API_URL, data).json()
+    return response["data"]
 
-with st.container():
-    st.session_state.answer = st.session_state.answer + [st.text_input('question', label_visibility='hidden', placeholder='Pergunta-me algo...')]
-    st.write(st.session_state.answer)
+
+# Define function to get API response
+def get_api_response(input_text):
+    response = requests.post(API_URL, json={
+        "data": [
+            input_text,
+        ]
+    }).json()
+    return response["data"][0]
+
+
+# Define Streamlit app
+def main():
+    st.title('Chat:blue[MFG]')
+    conversation_history = []
+    user_input = st.text_input("Pergunta-me algo", "")
+    if st.button("Send"):
+        api_response = get_api_response(user_input)
+        conversation_history.append(("You", user_input))
+        conversation_history.append(("Chatbot", api_response))
+    for speaker, text in conversation_history:
+        st.text(f"{speaker}: {text}")
+
+if __name__ == "__main__":
+    main()
