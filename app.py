114772a748a230d0331412ccaab720922af69193
Diogo Carapito <43778648+DiogoCarapito@users.noreply.github.com>
2023-04-21 12:51:37 +0100

    interface tests

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
 import requests
 import streamlit as st
+#from streamlit_chat import message
 
 # Define API endpoint
 API_URL = 'https://diogocarapito-chatmgf.hf.space/run/predict'
 
+# Define function to get API response
 def get_api_response(input_text):
-    data = {"input_text": input_text}
-    response = requests.post(API_URL, data).json()
+    response = requests.post(API_URL, json={"data": [input_text,]}).json()
+    #return response["data"][0]
     return response["data"]
 
 
-# Define function to get API response
-def get_api_response(input_text):
-    response = requests.post(API_URL, json={
-        "data": [
-            input_text,
-        ]
-    }).json()
-    return response["data"][0]
-
-
 # Define Streamlit app
-def main():
-    st.title('Chat:blue[MFG]')
-    conversation_history = []
-    user_input = st.text_input("Pergunta-me algo", "")
-    if st.button("Send"):
-        api_response = get_api_response(user_input)
-        conversation_history.append(("You", user_input))
-        conversation_history.append(("Chatbot", api_response))
-    for speaker, text in conversation_history:
-        st.text(f"{speaker}: {text}")
+#st.title('Chat:blue[MFG]')
+st.title(':blue[MFG]')
+conversation_history = []
 
-if __name__ == "__main__":
-    main()
+if "conversation_history" not in st.session_state:
+    st.session_state.conversation_history = []
+
+user_input = st.text_input("Pergunta-me algo", "")
+if st.button("Send"):
+    api_response = get_api_response(user_input)
+    st.session_state.conversation_history.append(("You", user_input))
+    st.session_state.conversation_history.append(("Chatbot", api_response))
+for speaker, text in st.session_state.conversation_history:
+    st.text(f"{speaker}: {text}")
+
